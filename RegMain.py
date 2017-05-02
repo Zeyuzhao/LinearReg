400a3b7c73ab8ac65830bbbdf66bd3d31f53776c
zachzhao <Zachzhao@outlook.com>
2017-05-02 07:50:07 -0400

    MatPlotLib adjustment

--- RegMain.py
+++ RegMain.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib import style
+#from matplotlib import style
 from LinearRegClass import *
 import time
 
@@ -10,7 +10,7 @@ time.sleep(1)
 print('2')
 time.sleep(1)
 print('1')
-style.use('ggplot')
+#style.use('ggplot')
 
 data = np.loadtxt('data.txt', delimiter=',',ndmin=2)
 
